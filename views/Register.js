c106884fc5a90ef22dd946d3d866fd3e1e3625a0
quentin.lepicier <quentin.lepicier@epitech.eu>
2021-04-26 14:41:58 +0200

    update Register

--- views/Register.js
+++ views/Register.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { TextInput as PaperInput} from 'react-native-paper';
 
 export function handleRegister(navigation, textEmail, textPassword, textEmailVerification,
      textPasswordVerification/*, textLastName, textFirstName*/) {
@@ -45,43 +46,58 @@ export default function Register({ homeStyle, navigation }) {
     const [passwordVerification, onChangePasswordVerification] = React.useState("");
 
     return (
-        <View style={homeStyle}>
-            <TextInput
+        <View style={styles.container}>
+            <View style={styles.fields}>
+            <PaperInput
                 title="Nom"
+                label="Nom"
+                mode="outlined"
                 placeholder="Nom..."
                 onChangeText={(text) => onChangeLastName(text)}
                 value={lastName}>
-            </TextInput>
-            <TextInput
+            </PaperInput>
+            <PaperInput
                 title="Prénom"
+                label="Prénom"
+                mode="outlined"
                 placeholder="Prénom..."
                 onChangeText={(text) => onChangeFirstName(text)}
                 value={firstName}>
-            </TextInput>
-            <TextInput
+            </PaperInput>
+            <PaperInput
                 title="Mot de Passe"
+                label="Mot de Passe"
+                mode="outlined"
                 placeholder="Mot de Passe..."
                 onChangeText={(text) => onChangePassword(text)}
                 value={password}>
-            </TextInput>
-            <TextInput
+            </PaperInput>
+            <PaperInput
                 title="Confirmation Mot de Passe"
+                label="Confirmation Mot de Passe"
+                mode="outlined"
                 placeholder="Confirmation Mot de Passe..."
                 onChangeText={(text) => onChangePasswordVerification(text)}
                 value={passwordVerification}>
-            </TextInput>
-            <TextInput
+            </PaperInput>
+            <PaperInput
                 title="Addresse Email"
+                label="Addresse Email"
+                mode="outlined"
                 placeholder="Addresse Email..."
                 onChangeText={(text) => onChangeEmail(text)}
                 value={email}>
-            </TextInput>
-            <TextInput
+            </PaperInput>
+            <PaperInput
                 title="Confirmation Addresse Email"
+                label="Confirmation Addresse Email"
+                mode="outlined"
                 placeholder="Confirmation Addresse Email..."
                 onChangeText={(text) => onChangeEmailVerification(text)}
                 value={emailVerification}>
-            </TextInput>
+            </PaperInput>
+            </View>
+            <View>
             <Button
                 onPress={() => handleRegister(navigation, email, password, emailVerification,
                                             passwordVerification/*, lastName, firstName*/)}
@@ -92,6 +108,28 @@ export default function Register({ homeStyle, navigation }) {
                 onPress={() =>
                     navigation.navigate("Connection")}>
             </Button>
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    fields: {
+        flex: 2,
+        padding: 10,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    button: {
+        flex: 2,
+        padding: 10,
+        alignItems: "evenly-spaced",
+        justifyContent: "center",
+    },
+});
